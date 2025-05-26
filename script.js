50481260d2113c3470fe56d5fbe112e339b782a5
ElevateRacing <m.michalczyk@sky-ride.com>
2025-05-26 09:38:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+window.addEventListener('scroll', () => {
+            const navbox = document.querySelector('.navbar');
+            const logo = document.querySelector('.logo');
+            if (window.scrollY > 50) {
+                logo.classList.add('shrink');
+                navbox.classList.add('scrolled');
+            } else {
+                logo.classList.remove('shrink');
+                navbox.classList.remove('scrolled');
+            }
+        });
